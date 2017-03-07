f1233db6bfc0e7dfe3b536d7b45172d8fe097b03
Olivier Truong <oliv.truong@gmail.com>
2017-03-07 22:03:36 +0100

    Add json output

--- src/test/java/com/oltruong/demo/RunCukesIT.java
+++ src/test/java/com/oltruong/demo/RunCukesIT.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:features", format = {"html:target/cucumber-html-report"})
+@CucumberOptions(features = "classpath:features", format = {"html:target/cucumber-html-report","json:target/cucumber.json"})
 public class RunCukesIT {
 
 
